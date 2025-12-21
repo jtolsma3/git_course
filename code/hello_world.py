73b8b4bbab243010b69a5d94e640bb6b704823ff
Joe Tolsma <tolsmajdx@gmail.com>
2025-12-21 14:48:45 -0800

    Add total days of life computation using utils.py file

--- code/hello_world.py
+++ code/hello_world.py
@@ -1,5 +1,5 @@
 import datetime
-import zoneinfo
+import utils
 
 # compute system timezone 
 aware_local_now = datetime.datetime.now().astimezone()
@@ -9,6 +9,12 @@ local_tz = aware_local_now.tzinfo
 christmas = datetime.datetime(year = datetime.datetime.now().year, month = 12, day = 25)
 days_to_christmas = (christmas - datetime.datetime.now()).days + 1
 
+# compute days of life
+birthdate = utils.get_birthdate()
+days_of_life = (datetime.datetime.now() - birthdate).days + 1 
 
+print("")
 print(f"It's {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")} in {local_tz} time zone and I would just like to say, 'Hello World!!!'")
 print(f"It's {days_to_christmas} days until Christmas!")
+print(f"I have lived a total of {days_of_life} days!!")
+print("")
---
+++ code/utils.py
@@ -0,0 +1,4 @@
+import datetime
+
+def get_birthdate():
+    return datetime.datetime(year = 1986, month = 7, day = 19)
