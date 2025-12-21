818e860de23747001feaf036a36ee853a07fd85a
Joe Tolsma <tolsmajdx@gmail.com>
2025-12-21 14:37:49 -0800

    Add total days of life computation

--- code/hello_world.py
+++ code/hello_world.py
@@ -9,6 +9,12 @@ local_tz = aware_local_now.tzinfo
 christmas = datetime.datetime(year = datetime.datetime.now().year, month = 12, day = 25)
 days_to_christmas = (christmas - datetime.datetime.now()).days + 1
 
+# compute total days of life
+birthdate = datetime.datetime(year = 1986, month = 7, day = 19)
+days_of_life = (datetime.datetime.now() - birthdate).days + 1
 
+print("")
 print(f"It's {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")} in {local_tz} time zone and I would just like to say, 'Hello World!!!'")
 print(f"It's {days_to_christmas} days until Christmas!")
+print(f"I have been alive for {days_of_life} total days!!")
+print("")
