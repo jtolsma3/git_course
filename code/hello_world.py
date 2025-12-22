e92173099be5ec02f03e700edfc6c3e48636870b
Joe Tolsma <tolsmajdx@gmail.com>
2025-12-22 13:36:44 -0800

    replace Magna Carta with American and French Revolutions

--- code/hello_world.py
+++ code/hello_world.py
@@ -21,10 +21,15 @@ days_of_life_m = (datetime.datetime.now() - birthdate_m).days + 1
 birthdate_d = utils.get_birthdate(person = "dad")
 days_of_life_d = (datetime.datetime.now() - birthdate_d).days + 1
 
-# compute time since a particular event
-event = "magna_carta"
+# compute time the American Revolution
+event = "american_independence"
 year = datetime.datetime(year = utils.get_event_year(event),month = 1, day = 1)
 year_gap = math.ceil((datetime.datetime.now() - year).days/365.25)
+
+# compute time since the French Revolution
+event2 = "french_revolution"
+year2 = datetime.datetime(year = utils.get_event_year(event2),month = 1,day = 1)
+year2_gap = math.ceil((datetime.datetime.now() - year2).days/365.25)
 
 print("")
 print(f"It's {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")} in {local_tz} time zone and I would just like to say, 'Hello World!!!'")
@@ -32,4 +37,5 @@ print(f"It's {days_to_christmas} days until Christmas!")
 print(f"I have lived a total of {days_of_life} days!!")
 print(f"Mom has lived {days_of_life_m} days and dad has lived {days_of_life_d} days!!")
 print(f"The {event.replace("_"," ").title()} happened {year_gap} years ago!!")
+print(f"The {event2.replace("_"," ").title()} happened {year2_gap} years ago!!")
 print("")
