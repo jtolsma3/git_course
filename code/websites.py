b17c6600eabf6c4c5f73ed788cd1dbe6a9718549
jtolsma3 <macrame_fend.7a@icloud.com>
2025-12-27 14:42:42 -0800

    Create websites.py

---
+++ code/websites.py
@@ -0,0 +1,11 @@
+# list of websites that I visit sometimes
+
+[
+https://www.defector.com/,
+https://www.deadspin.com/,
+https://mail.google.com/,
+https://www.udemy.com/,
+https://www.wallstreetprep.com/,
+https://www.kaggle.com/,
+https://www.github.com/,
+]
