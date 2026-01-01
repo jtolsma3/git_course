9d2f3fc7f91d38cff1a8b5932f7cd4fb02bb64e9
jtolsma3 <macrame_fend.7a@icloud.com>
2026-01-01 13:42:29 -0800

    Add nato alphabet to empty.py

--- code/empty.py
+++ code/empty.py
@@ -1,3 +1,6 @@
 # nothing here
 # this is just an empty file that I can commit and pull changes
 # the quick brown fox jumped over the lazy dog
+
+# NATO alphabet: alpha, bravo, charlie, delta, echo, foxtrot, golf, hotel, india, juliet, kilo, lima, m, november, oscar, p, q, r, sierra, 
+# t, u, victor, w, x-ray, yankee, zulu
